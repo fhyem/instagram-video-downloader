599ef7345a39fe4c94ff1a395255903256611a86
fhyem <fhyembercha@gmail.com>
2023-01-31 23:33:59 +0500

    boiler plate nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [input, setInput] = useState("")
   return (
     <div className={styles.container}>
       <Head>
@@ -11,41 +13,12 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Download <h4>Instagram Videos!</h4>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div style={{backgroundColor: "red", width: "800px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+          <input style={{ width: "78%", height: "50px"}} placeholder='Please paste the video URL' value={input} onChange={(e) => setInput(e.target.value)} />
+          <button style={{ width: "22%", height: "50px"}} onClick={() => console.log("Submitted")}>Download</button>
         </div>
       </main>
 
